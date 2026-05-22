981af6f081adab025f1fd4e4abf3e07765560719
iamthevgod <dhatway2024@gmail.com>
2026-05-21 21:08:24 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'edge-v1';
+const CACHE = 'edge-v2';
 
 const STATIC = [
   '/',
@@ -14,34 +14,6 @@ const STATIC = [
   '/betting.html',
   '/settings.html',
   '/admin.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/engine.js'
 ];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(STATIC))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  // Network first for API calls, cache first for static
-  if (e.request.url.includes('api.')) {
-    e.respondWith(
-      fetch(e.request).catch(() => caches.match(e.request))
-    );
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(cached => cached || fetch(e.request))
-    );
-  }
-});
